087044bc567c350e46e8881a2a8b0c7423a03630
Anderson Severo <severowz@gmail.com>
2021-09-06 15:58:26 -0300

    Adicionando e corrigindo as funções de drag and drop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,63 @@
-html{
-    font-family: 'Azeret Mono', monospace;
+/* Logs */
+function log(message){
+    console.log(message);
+}
+
+/* Cards de instruções */
+
+const cards = document.querySelectorAll('.card');
+const dropzones = document.querySelectorAll('.dropzone');
+
+cards.forEach(function(card){
+    card.addEventListener('dragstart', dragstart);
+    card.addEventListener('drag', drag);
+    card.addEventListener('dragend', dragend);
+});
+
+ function dragstart(){
+    dropzones.forEach(function(dropzone){
+        dropzone.classList.add('highlight');
+    });
+    this.classList.add('dragging'); 
+
+}
+
+function drag(){
+}
+
+function dragend(){
+    dropzones.forEach(function(dropzone){
+        dropzone.classList.remove('highlight');
+    });
+    this.classList.remove('dragging'); 
+} 
+
+/* Dropzones */
+
+dropzones.forEach(function(dropzone){
+    dropzone.addEventListener('dragenter', dragenter);
+    dropzone.addEventListener('dragover', dragover);
+    dropzone.addEventListener('dragleave', dragleave);
+    dropzone.addEventListener('dragleave', dragleave);
+    dropzone.addEventListener('drop', drop);
+});
+
+function dragenter(){
+}
+
+function dragover(){
+    this.classList.add('over');
+
+    const cardBeingDragged = document.querySelector('.dragging');
+
+    this.appendChild(cardBeingDragged)
+}
+
+function dragleave(){
+    this.classList.remove('over')
+
+}
+
+function drop(){
+    this.classList.remove('over')
 }
